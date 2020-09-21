f1df0a79a448c2894d80f8c3442a8b1d7095f2e9
Morten Brudvik <morten@brudvik.com>
2020-09-21 09:11:42 +0200

    First version with mutext support for detecting if single instance running

---
+++ app2/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using shared;
+
+namespace app2
+{ 
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var instanceService = new SingleInstanceService();
+            if (!instanceService.Start())
+            {
+                Console.Out.WriteLine("There is all ready another instance of this application running");
+            }
+            else
+            {
+                Console.Out.WriteLine("This is the first instance of this application running");
+            }
+
+            Console.ReadKey();
+            instanceService.Stop();
+        }
+    }
+}
---
+++ shared/SingleInstanceService.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Threading;
+
+namespace shared
+{
+    /// <summary>
+    /// Features:
+    ///     Sets a local mutext for the first running instance.
+    ///     The option to send command(s) from the second instance the first.
+    ///
+    /// </summary>
+    public class SingleInstanceService : IDisposable
+    {
+        private Mutex _mutex;
+        
+        public bool Start()
+        {
+            if (_mutex != null)
+                throw new InvalidOperationException("Mutext has all ready been set. Please call Stop to release the mutext.");
+
+            _mutex = new Mutex(true, "some-unique_id-" + Environment.UserName, out var firstInstance);
+
+            return firstInstance;
+        }
+
+        public void Stop()
+        {
+            Dispose();
+        }
+
+        public void Dispose()
+        {
+            if (_mutex == null) return;
+            
+            _mutex.ReleaseMutex();
+            _mutex.Close();
+            _mutex = null;
+        }
+    }
+}
